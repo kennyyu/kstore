e06e8956b74da61c54f407b577268ef3118fe444
Kenny Yu <yu.kenny124@gmail.com>
2014-02-21 16:11:54 -0500

    add list

---
+++ src/common/include/list.h
@@ -0,0 +1,120 @@
+#ifndef _COMMON_LIST_H
+#define _COMMON_LIST_H
+
+struct listnode;
+struct list;
+
+struct list *list_create(void);
+void list_destroy(struct list *lst);
+unsigned list_size(struct list *lst);
+int list_addhead(struct list *lst, void *item);
+int list_addtail(struct list *lst, void *item);
+void *list_gethead(struct list *lst);
+void *list_gettail(struct list *lst);
+void *list_remhead(struct list *lst);
+void *list_remtail(struct list *lst);
+
+// returns NULL if there are no more items left
+struct listnode *list_iterhead(struct list *lst);
+struct listnode *list_itertail(struct list *lst);
+struct listnode *list_next(struct listnode *lnode);
+struct listnode *list_prev(struct listnode *lnode);
+void *list_getentry(struct listnode *lnode);
+
+/*
+ * EXAMPLE USE:
+ *
+ * struct foo {};
+ * DECLLIST(foo);
+ * DEFLIST(foo);
+ * for (struct foolistnode *lnode = foolist_iterhead(lst);
+ *      lnode != NULL; lnode = foolist_next(lnode)) {
+ *     struct foo *item = foolist_getentry(lnode);
+ *     ...
+ * }
+ */
+
+#define DECLLIST_BYTYPE(LIST, T) \
+    struct LIST; \
+    struct LIST##node; \
+    struct LIST *LIST##_create(void); \
+    void LIST##_destroy(struct LIST *lst); \
+    unsigned LIST##_size(struct LIST *lst); \
+    int LIST##_addhead(struct LIST *lst, T *item); \
+    int LIST##_addtail(struct LIST *lst, T *item); \
+    T *LIST##_gethead(struct LIST *lst); \
+    T *LIST##_gettail(struct LIST *lst); \
+    T *LIST##_remhead(struct LIST *lst); \
+    T *LIST##_remtail(struct LIST *lst); \
+    struct LIST##node *LIST##_iterhead(struct LIST *lst); \
+    struct LIST##node *LIST##_itertail(struct LIST *lst); \
+    struct LIST##node *LIST##_next(struct LIST##node *lnode); \
+    struct LIST##node *LIST##_prev(struct LIST##node *lnode); \
+    T *LIST##_getentry(struct LIST##node *lnode);
+
+#define DEFLIST_BYTYPE(LIST, T) \
+    struct LIST { \
+        struct list *lst; \
+    }; \
+    struct LIST##node {}; \
+    struct LIST *LIST##_create(void) { \
+        struct LIST *lst = malloc(sizeof(struct LIST)); \
+        if (lst == NULL) { \
+            goto done; \
+        } \
+        lst->lst = list_create(); \
+        if (lst->lst == NULL) { \
+            goto cleanup_malloc; \
+        } \
+        goto done; \
+      cleanup_malloc: \
+        free(lst); \
+        lst = NULL; \
+      done: \
+        return lst; \
+    } \
+    void LIST##_destroy(struct LIST *lst) { \
+        list_destroy(lst->lst); \
+        free(lst); \
+    } \
+    unsigned LIST##_size(struct LIST *lst) { \
+        return list_size(lst->lst); \
+    } \
+    int LIST##_addhead(struct LIST *lst, T *item) { \
+        return list_addhead(lst->lst, (void *) item); \
+    }\
+    int LIST##_addtail(struct LIST *lst, T *item) { \
+        return list_addtail(lst->lst, (void *) item); \
+    }\
+    T *LIST##_gethead(struct LIST *lst) { \
+        return list_gethead(lst->lst); \
+    }\
+    T *LIST##_gettail(struct LIST *lst) { \
+        return list_gettail(lst->lst); \
+    } \
+    T *LIST##_remhead(struct LIST *lst) { \
+        return list_remhead(lst->lst); \
+    } \
+    T *LIST##_remtail(struct LIST *lst) { \
+        return list_remtail(lst->lst); \
+    } \
+    struct LIST##node *LIST##_iterhead(struct LIST *lst) { \
+        return (struct LIST##node *) list_iterhead(lst->lst); \
+    }\
+    struct LIST##node *LIST##_itertail(struct LIST *lst) { \
+        return (struct LIST##node *) list_itertail(lst->lst); \
+    } \
+    struct LIST##node *LIST##_next(struct LIST##node *lnode) { \
+        return (struct LIST##node *) list_next((struct listnode *) lnode); \
+    } \
+    struct LIST##node *LIST##_prev(struct LIST##node *lnode) { \
+        return (struct LIST##node *) list_prev((struct listnode *) lnode); \
+    } \
+    T *LIST##_getentry(struct LIST##node *lnode) { \
+        return (T *) list_getentry((struct listnode *) lnode); \
+    }
+
+#define DECLLIST(T) DECLLIST_BYTYPE(T##list, struct T)
+#define DEFLIST(T) DEFLIST_BYTYPE(T##list, struct T)
+
+#endif
---
+++ src/common/list.c
@@ -0,0 +1,197 @@
+#include <assert.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <errno.h>
+#include "include/list.h"
+
+struct listnode {
+    struct listnode *ln_next;
+    struct listnode *ln_prev;
+    void *ln_item;
+};
+
+struct list {
+    struct listnode lst_head;
+    struct listnode lst_tail;
+    unsigned int lst_size;
+};
+
+struct list *
+list_create(void)
+{
+    struct list *lst = malloc(sizeof(struct list));
+    if (lst == NULL) {
+        goto done;
+    }
+    // initialize the head and tail to point to each other
+    lst->lst_head.ln_prev = NULL;
+    lst->lst_head.ln_next = &lst->lst_tail;
+    lst->lst_tail.ln_next = NULL;
+    lst->lst_head.ln_prev = &lst->lst_head;
+    lst->lst_size = 0;
+  done:
+    return lst;
+}
+
+void
+list_destroy(struct list *lst)
+{
+    assert(lst != NULL);
+    // we only want to destroy empty lists to ensure that the
+    // user has freed any objects referenced in this list
+    assert(list_size(lst) == 0);
+    free(lst);
+}
+
+unsigned list_size(struct list *lst) {
+    assert(lst != NULL);
+    return lst->lst_size;
+}
+
+int
+list_addhead(struct list *lst, void *item)
+{
+    assert(lst != NULL);
+    int result;
+    struct listnode *lnode = malloc(sizeof(struct listnode));
+    if (lnode == NULL) {
+        result = ENOMEM;
+        goto done;
+    }
+    lnode->ln_item = item;
+    lnode->ln_next = lst->lst_head.ln_next;
+    lnode->ln_next->ln_prev = lnode;
+    lnode->ln_prev = &lst->lst_head;
+    lnode->ln_prev->ln_next = lnode;
+    lst->lst_size++;
+    result = 0;
+  done:
+    return result;
+}
+
+int
+list_addtail(struct list *lst, void *item)
+{
+    assert(lst != NULL);
+    int result;
+    struct listnode *lnode = malloc(sizeof(struct listnode));
+    if (lnode == NULL) {
+        result = ENOMEM;
+        goto done;
+    }
+    lnode->ln_item = item;
+    lnode->ln_next = lst->lst_head.ln_next;
+    lnode->ln_next->ln_prev = lnode;
+    lnode->ln_prev = &lst->lst_head;
+    lnode->ln_prev->ln_next = lnode;
+    lst->lst_size++;
+    result = 0;
+  done:
+    return result;
+}
+
+void *
+list_gethead(struct list *lst)
+{
+    assert(lst != NULL);
+    if (lst->lst_size == 0) {
+        return NULL;
+    }
+    return lst->lst_head.ln_next->ln_item;
+}
+
+void *
+list_gettail(struct list *lst)
+{
+    assert(lst != NULL);
+    if (lst->lst_size == 0) {
+        return NULL;
+    }
+    return lst->lst_tail.ln_prev->ln_item;
+}
+
+void *
+list_remhead(struct list *lst)
+{
+    assert(lst != NULL);
+    assert(lst->lst_size > 0);
+    struct listnode *lnode = lst->lst_head.ln_next;
+    void *item = lnode->ln_item;
+    lnode->ln_next->ln_prev = lnode->ln_prev;
+    lnode->ln_prev->ln_next = lnode->ln_next;
+    free(lnode);
+    lst->lst_size--;
+    return item;
+}
+
+void *
+list_remtail(struct list *lst)
+{
+    assert(lst != NULL);
+    assert(lst->lst_size > 0);
+    struct listnode *lnode = lst->lst_tail.ln_prev;
+    void *item = lnode->ln_item;
+    lnode->ln_next->ln_prev = lnode->ln_prev;
+    lnode->ln_prev->ln_next = lnode->ln_next;
+    free(lnode);
+    lst->lst_size--;
+    return item;
+}
+
+// returns NULL if there are no more items left
+struct listnode *
+list_iterhead(struct list *lst)
+{
+    assert(lst != NULL);
+    if (lst->lst_size == 0) {
+        return NULL;
+    }
+    assert(lst->lst_head.ln_next != &lst->lst_tail);
+    assert(lst->lst_tail.ln_prev != &lst->lst_head);
+    return lst->lst_head.ln_next;
+}
+
+struct listnode *
+list_itertail(struct list *lst)
+{
+    assert(lst != NULL);
+    if (lst->lst_size == 0) {
+        return NULL;
+    }
+    assert(lst->lst_head.ln_next != &lst->lst_tail);
+    assert(lst->lst_tail.ln_prev != &lst->lst_head);
+    return lst->lst_tail.ln_prev;
+}
+
+struct listnode *
+list_next(struct listnode *lnode)
+{
+    assert(lnode != NULL);
+    assert(lnode->ln_next != NULL);
+    assert(lnode->ln_prev != NULL);
+    if (lnode->ln_next->ln_next == NULL) {
+        return NULL;
+    }
+    return lnode->ln_next;
+}
+
+struct listnode *
+list_prev(struct listnode *lnode)
+{
+    assert(lnode != NULL);
+    assert(lnode->ln_next != NULL);
+    assert(lnode->ln_prev != NULL);
+    if (lnode->ln_prev->ln_prev == NULL) {
+        return NULL;
+    }
+    return lnode->ln_prev;
+}
+
+void *
+list_getentry(struct listnode *lnode)
+{
+    assert(lnode != NULL);
+    assert(lnode->ln_next != NULL);
+    assert(lnode->ln_prev != NULL);
+    return lnode->ln_item;
+}
